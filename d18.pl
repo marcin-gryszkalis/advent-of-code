4042ed0e51cac8b034e0c733462338bd1b3e5035
Marcin Gryszkalis <mg@fork.pl>
2019-12-23 02:34:23 +0100

    working for examples but not final (because of loop in the center)

--- d18.pl
+++ d18.pl
@@ -3,6 +3,7 @@ use 5.28.0;
 use warnings;
 use strict;
 use Data::Dumper;
+use List::Util qw/min max/;
 use Graph::Undirected;
 use Algorithm::Combinatorics qw(combinations);
 
@@ -20,8 +21,8 @@ my $starty;
 my $nok = 0; # number of keys
 my $nod = 0; # number of doors
 
-# my @zm = ( map { [(0) x ($sy+1)] } (0..($sx+1))  );
-# my $m = \@zm; # map
+my %cache;
+
 my $m;
 
 my $x = 0;
@@ -85,7 +86,6 @@ say "DOORS($nod)";
 
 $x = $startx;
 $y = $starty;
-
 
 my $mv = 0;
 my $step = 0;
@@ -184,14 +184,14 @@ while (1)
 		$y += $dy[$d];
 
 #		draw();
-#		print "The graph is $g\n";
+		print "The graph is $g\n";
 
 		$mv++;
 }
 
 
 my $ap_len;
-my $ap_required_keys;
+my $ap_required_keys = {};
 my $ap_doors_between;
 
 my @edges = $g->edges;
@@ -202,9 +202,12 @@ for my $e (@edges)
 	my $w = $g->get_edge_weight($a,$b);
 	printf "%7s - %7s = %3d\n",$a,$b,$w;
 
+	$ap_required_keys->{$a}->{$b} = [];
+	$ap_required_keys->{$b}->{$a} = [];
+
 }
 
-
+# optimize graph (remove crossroads and doors)
 my $modified = 1;
 while ($modified)
 {
@@ -212,7 +215,7 @@ while ($modified)
 
 	for my $v ($g->vertices())
 	{
-		next if $v eq '@' or $v =~ m/[a-z]/i;
+		next if $v =~ m/[a-z@]/;
 
 		my @nbs = $g->neighbours($v);
 		if (scalar @nbs == 1) # leaf node
@@ -223,43 +226,67 @@ while ($modified)
 			next;
 		}
 
-		say "OPTIMIZE-CROSSING($v)";
+		say "OPTIMIZE($v)";
 		my $it = combinations(\@nbs, 2);
 		while (my $es = $it->next)
 		{
+			next if $g->has_edge($es->[0], $es->[1]); # already connected
+
 			my $sumw =  $g->get_edge_weight($es->[0], $v) + $g->get_edge_weight($v, $es->[1]);
-			say "   $es->[0] --($sumw)-- $es->[1]";
+			say "   $es->[0] --$v($sumw)-- $es->[1]";
 			$g->add_weighted_edge($es->[0], $es->[1], $sumw);
+
+			my @pathy1 = ();
+
+			if ($v =~ m/[A-Z]/) # door
+			{
+				push(@pathy1, $v);
+			}
+			push(@pathy1, @{$ap_required_keys->{$es->[0]}->{$v}});
+			push(@pathy1, @{$ap_required_keys->{$v}->{$es->[1]}});
+
+			my @pathy2 = @pathy1;
+			$ap_required_keys->{$es->[0]}->{$es->[1]} = \@pathy1;
+			$ap_required_keys->{$es->[1]}->{$es->[0]} = \@pathy2;
+			# print Dumper $ap_required_keys;
 		}
 		$g->delete_vertex($v);
 		$modified = 1;
 	}
 }
-
+#	say Dumper $ap_ _keys;
 my @edges = $g->edges;
 for my $e (@edges)
 {
 	my $a = shift @$e;
 	my $b = shift @$e;
 	my $w = $g->get_edge_weight($a,$b);
-	printf "%7s - %7s = %3d\n",$a,$b,$w;
 
+	printf "%7s - %7s = %3d, req: %s\n",$a,$b,$w, join("", @{$ap_required_keys->{$a}->{$b}});
 }
 
+# my @kvs = grep { /[a-z@]/ } $g->vertices;
+my @kvs = $g->vertices;
 
 
-my @kvs = grep { $_ eq '@' || /[a-z]/ } $g->vertices;
+# all pairs
 my $it = combinations(\@kvs, 2);
 while (my $es = $it->next)
 {
 	my @path = $g->SP_Dijkstra($es->[0], $es->[1]);
 	my $l = 0;
 	my $ip = 0;
+	my @reqks = ();
 	while ($ip < $#path)
 	{
 		$l += $g->get_edge_weight($path[$ip], $path[$ip+1]);
+		push(@reqks, @{$ap_required_keys->{$path[$ip]}->{$path[$ip+1]}});
 		$ip++;
 	}
+
+	$ap_required_keys->{$es->[0]}->{$es->[1]} = \@reqks;
+	my @reqks2 = @reqks;
+	$ap_required_keys->{$es->[1]}->{$es->[0]} = \@reqks;
 
 	$ap_len->{$es->[0]}->{$es->[1]} =
 	$ap_len->{$es->[1]}->{$es->[0]} = $l;
@@ -267,23 +294,25 @@ while (my $es = $it->next)
 	$ap_len->{$es->[0]}->{$es->[0]} = 0; # we need that for sort later
 	$ap_len->{$es->[1]}->{$es->[1]} = 0; # we need that for sort later
 
-	my @pathx = grep { /[A-Z]/ } @path;
-	$ap_required_keys->{$es->[0]}->{$es->[1]} =
-	$ap_required_keys->{$es->[1]}->{$es->[0]} = \@pathx;
+	# my @pathx = grep { /[A-Z]/ } @path;
+#	$ap_required_keys->{$es->[0]}->{$es->[1]} =
+#	$ap_required_keys->{$es->[1]}->{$es->[0]} = \@pathx;
 
-	my @pathy = grep { /[a-z]/ } @path;
-	@pathy = @pathy[1..$#pathy-1]; # skip first and last
-	$ap_doors_between->{$es->[0]}->{$es->[1]} =
-	$ap_doors_between->{$es->[1]}->{$es->[0]} = \@pathy;
+	# my @pathy = grep { /[a-z]/ } @path;
+	# @pathy = @pathy[1..$#pathy-1]; # skip first and last
+	# $ap_doors_between->{$es->[0]}->{$es->[1]} =
+	# $ap_doors_between->{$es->[1]}->{$es->[0]} = \@pathy;
 }
 
 my $it = combinations(\@kvs, 2);
 while (my $es = $it->next)
 {
-	say "$es->[0] --(".($ap_len->{$es->[0]}->{$es->[1]}).")-- $es->[1], ".
-	"required: ".join(",", @{$ap_required_keys->{$es->[0]}->{$es->[1]}}).
-	", between: ".join(",", @{$ap_doors_between->{$es->[0]}->{$es->[1]}});
+	say " $es->[0] --(".($ap_len->{$es->[0]}->{$es->[1]}).")-- $es->[1], ".
+	"required: ".join(",", @{$ap_required_keys->{$es->[0]}->{$es->[1]}});#.
+	#", between: ".join(",", @{$ap_doors_between->{$es->[0]}->{$es->[1]}});
 }
+
+
 
 # DFS
 $mv = 0;
@@ -295,22 +324,32 @@ sub visit
 
 	my $c = shift; # current
 	my $pa = shift; # path list ref
-#	my $fkh = shift; # found keys ref
+	my $vh = shift; # found keys ref
 
-	# say "visit($c)";
+	say STDERR "$mv visit($c)" if $mv % 1000 == 0;
 	my @path = @$pa;
-
+	# my %visited = %$vh;
 
 #	my %found_keys = %$fkh;
 
 	my %vp1 = map { $_ => 1 } @path; # visited by path
-#	my $v1 = join("/", sort grep { /[a-z]/ } keys %vp1);
+	my $v1 = join("", sort grep { /[a-z@]/ } keys %vp1); # as sorted string
+
+	my $v3 = "$v1:$c";
+	if (exists $cache{$v3})
+	{
+		say STDERR "FOUND-IN-CACHE($v3) = $cache{$v3}";
+		return $cache{$v3};
+	}
 
 	push(@path, $c);
 #	die if scalar(@path) > 1000; # justin case
 
-	print "$mv CHECK: ".join(" ", @path)."\n"; # if $mv % 10000 == 0;
-	return if (scalar @path > $nok+1); # dupes in path, too long
+	my %vp2 = map { $_ => 1 } @path; # visited by path
+	my $v2 = join("", sort grep { /[a-z@]/ } keys %vp2);
+
+	printf STDERR "$mv CHECK: %s (vp1=%s vp2=%s)\n", join(" ", @path), $v1, $v2; # if $mv % 10000 == 0;
+	# return if (scalar @path > $nok+1); # dupes in path, too long
 
 	my $xxp = join("",@path);
 
@@ -323,152 +362,82 @@ sub visit
 		$l += $ap_len->{$path[$ip]}->{$path[$ip+1]};
 		$ip++;
 	}
-	return if $l > $shortest; # too long
+	return $l if $l > $shortest; # too long
 
 	# check for success - count keys
 
+	my $res = 0;
 	if (scalar @path == $nok+1)
 	{
 		# success!
-		print "$mv GOOD: ".join(" ", @path)." = $l\n";
+		print STDERR "$mv GOOD: ".join(" ", @path)." = $l\n";
 
-		# print "LEN: $l\n";
-		if ($l < $shortest)
-		{
-			# my %seen;
-			# @shortestp = grep { /[a-z]/ && !$seen{$_}++} @path;
-			@shortestp = @path;
-			$shortest = $l;
-			print "\nSHORTEST: ".join(" ", @shortestp)." = $shortest\n";
-		}
+		# # print "LEN: $l\n";
+		# if ($l < $shortest)
+		# {
+		# 	# my %seen;
+		# 	# @shortestp = grep { /[a-z]/ && !$seen{$_}++} @path;
+		# 	@shortestp = @path;
+		# 	$shortest = $l;
+		# 	print "\nSHORTEST: ".join(" ", @shortestp)." = $shortest\n";
+		# }
 
-		return;
+		$res = 0;
 	}
-
-	NN: for my $next (sort { $ap_len->{$c}->{$a} <=> $ap_len->{$c}->{$b} } @kvs)
+	else
 	{
-	#	exit;
-		next if $c eq $next; # or check vp2
-		next if $vp1{$next}; # visited
-		for my $k (@{$ap_required_keys->{$c}->{$next}})
-		{
-#			print "goto $c -> $next, required key: $k\n";# if $xxp =~ /^afb/;
-			next NN unless $vp1{lc $k};
-#			print "OK\n";
+
+	 	my @tnext = ();
+	 	my $vi = 0;
+
+	# 	my @nbs = $g->neighbours($c);
+	#	NN: for my $n (sort { $g->get_edge_weight($a,$c) <=> $g->get_edge_weight($b,$c) } @kvs)
+
+		NN: for my $n (sort { $ap_len->{$a}->{$c} <=> $ap_len->{$b}->{$c} } @kvs)
+	 	{
+	 		$vi++;
+
+	 		next if $c eq $n;
+	#say "$c $n";
+	  		printf STDERR "$mv:$vi [$v2] try($c -> $n) req: %s (have: %s)\n", join("", @{$ap_required_keys->{$c}->{$n}}), $v2;
+	 		for my $k (@{$ap_required_keys->{$c}->{$n}})
+	 		{
+	# #			print "goto $c -> $next, required key: $k\n";# if $xxp =~ /^afb/;
+	 			next NN unless $vp2{lc $k};
+	# #			print "OK\n";
+	 		}
+
+	 		next if exists $vp2{$n}; # already in path...
+
+			my $vl = $ap_len->{$c}->{$n} + visit($n, \@path); # path with c
+			push(@tnext, $vl);
+
+			# if ($visited{"$v2/$c"})
+			# {
+			# 	say "already visited $v2";
+			# 	next;
+			# }
+			# $visited{$v2} = 1;
+			# # say "goto $c -> $n";
+			# visit($n, \@path, \%visited);
 		}
 
-		for my $vb (@{$ap_doors_between->{$c}->{$next}})
-		{
-			# next NN if $vp1{$vb}; # ?
-			push(@path, $vb) unless $vp1{$vb};
-		}
-
-		visit($next, \@path);
+		$res = min(@tnext);
+		say STDERR "TNEXT: $res = min: ".join(" ", @tnext);
 	}
+
+	say STDERR "CACHE-UPDATE: [$v2] $v3 = $res";
+	$cache{$v3} = $res;
+
+	return $res;
+
 }
 
 
 my @path = ();
 my %visited = ();
-visit('@', \@path);
-
-print "SHORTEST: ".join(" ", @shortestp)." = $shortest\n";
-
-
+# visit('@', \@path, \%visited);
+my $ret = visit('@', \@path);
+print "RET: $ret\n";
 
 
-
-
-
-exit;
-
-$mv = 0;
-my @shortestp;
-my $shortest = 100000;
-sub visit_
-{
-	$mv++;
-
-	my $c = shift; # current
-	my $vh = shift; # visited hash ref
-	my $pa = shift; # path list ref
-
-	# say "visit($c)";
-	my %visited = %$vh;
-	my @path = @$pa;
-
-#	print "(".join(" ", @path).") + $c\n";
-
-	my %vp1 = map { $_ => 1 } @path; # visited by path
-	my $v1 = join("/", sort grep { /[a-z]/ } keys %vp1);
-
-	push(@path, $c);
-	die if scalar(@path) > 1000; # justin case
-
-	my $ip = 0;
-	my $l = 0;
-	while ($ip < $#path)
-	{
-		$l += $g->get_edge_weight($path[$ip], $path[$ip+1]);
-		$ip++;
-	}
-	return if $l > $shortest;
-# say "LEM($l)";
-
-	print "$mv CHECK: ".substr(join(" ", @path),0,100)."... = $l\r" if $mv % 10000 == 0;
-
-	my %vp2 = map { $_ => 1 } @path; # visited by path
-	my $v2 = join("/", sort grep { /[a-z]/ } keys %vp2);
-
-	# check for loop
-	if (exists $visited{$c} && $visited{$c} eq $v2)
-	{
-		return; # we've been here with the same set of visited keys (not places)
-	}
-	$visited{$c} = $v2;
-
-	# check for success - count keys
-	my %fkeys = map { $_ => 1 } grep { /[a-z]/ } @path;
-	if (scalar keys %fkeys == $nok)
-	{
-		# success!
-
-		# print "LEN: $l\n";
-		if ($l < $shortest)
-		{
-			my %seen;
-			@shortestp = grep { /[a-z]/ && !$seen{$_}++} @path;
-			$shortest = $l;
-			print "\nSHORTEST: ".join(" ", @shortestp)." = $shortest\n";
-			return;
-		}
-	}
-
-	# found key
-	if ($c =~ /[a-z]/)
-	{
-		# already added to path
-	}
-
-	if ($c =~ /[A-Z]/)
-	{
-		my $k = lc $c;
-		return unless exists $fkeys{$k}; # no key, go back
-	}
-
-	my @nbs = $g->neighbours($c);
-	for my $n (sort { $g->get_edge_weight($a,$c) <=> $g->get_edge_weight($b,$c) } @nbs)
-	{
-		# say "goto $c -> $n";
-		visit($n, \%visited, \@path);
-	}
-}
-
-my @path = ();
-my %visited = ();
-#push(@path, '@');
-#$visited{'@'} = '';
-
-visit('@', \%visited, \@path);
-
-print "SHORTEST: ".join(" ", @shortestp)." = $shortest\n";
