76d1b3ab999d210c7673152ae4fc1a9d92890582
Marcin Gryszkalis <mg@fork.pl>
2019-12-26 04:14:44 +0100

    d22 part 2 without repetitions

---
+++ d22b.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl
+use 5.28.0;
+use warnings;
+use strict;
+use Data::Dumper;
+
+my @commands = ();
+open(my $f, "d22x.txt") or die $!;
+while (<$f>)
+{
+	chomp;
+	push(@commands, $_);
+}
+
+my $size = 10; #007;
+# $size = 119315717514047;
+my $rep = 101741582076661;
+
+my @deck = (0..$size-1);
+# printf("deck: %s\n", join(" ", @deck));
+
+# https://rosettacode.org/wiki/Modular_inverse#Perl
+sub invmod
+{
+	my($a,$n) = @_;
+	my($t,$nt,$r,$nr) = (0, 1, $n, $a % $n);
+	while ($nr != 0)
+	{
+		# Use this instead of int($r/$nr) to get exact unsigned integer answers
+		my $quot = int( ($r - ($r % $nr)) / $nr );
+		($nt,$t) = ($t-$quot*$nt,$nt);
+		($nr,$r) = ($r-$quot*$nr,$nr);
+	}
+
+	return if $r > 1; # can't find
+	$t += $n if $t < 0;
+	return $t;
+}
+
+my $p = 0; # 9 for d22x.txt example, Result: 9 2 5 8 1 4 7 0 3 6
+for my $c (reverse @commands)
+{
+	say $c;
+	if ($c =~ /deal with increment (\d+)/)
+	{
+		my $incr = $1;
+		$p = ($p * invmod($incr, $size)) % $size;
+	}
+	elsif ($c =~ /cut (-?\d+)/)
+	{
+		my $cut = $1;
+		$p = ($p + $cut + $size) % $size;
+	}
+	elsif ($c =~ /deal into new stack/)
+	{
+		$p = $size - $p - 1;
+	}
+	else
+	{
+		die;
+	}
+}
+say $p;
+
