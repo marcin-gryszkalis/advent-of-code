e15c1336441ea5b0524872eace261d0911e1a65b
Marcin Gryszkalis <mg@fork.pl>
2020-10-26 02:58:15 +0100

    cleanup

--- 2015/d02.pl
+++ 2015/d02.pl
@@ -1,22 +1,17 @@
 #!/usr/bin/perl
 use Data::Dumper;
+use warnings;
+use strict;
 
 my $total = 0;
 my $total_ribbon = 0;
 while (<>)
 {
     chomp;
-    ($l,$w,$h) = split/x/;
-    @a = sort { $a <=> $b } ($l,$h,$w);
-    @b = sort { $a <=> $b } ($l * $w, $w * $h, $h * $l);
-    $slack = $b[0];
-    my $sum = 2*$b[0] + 2*$b[1] + 2*$b[2] + $slack;
-    $total += $sum;
-
-    my $ribbon = 2*$a[0] + 2*$a[1] + $a[0]*$a[1]*$a[2];
-#    print join("x",@b)."\n";
-#    printf "%d %d %d\n",2*$b[0],2*$b[1],$b[0]*$b[1]*$b[2];
-    $total_ribbon += $ribbon;
+    my @a = sort { $a <=> $b } split/x/;
+    my @b = sort { $a <=> $b } ($a[0] * $a[1], $a[1] * $a[2], $a[2] * $a[0]);
+    $total += 2*$b[0] + 2*$b[1] + 2*$b[2] + $b[0];
+    $total_ribbon += 2*$a[0] + 2*$a[1] + $a[0]*$a[1]*$a[2];
 }
 
 print "stage 1: $total\n";
