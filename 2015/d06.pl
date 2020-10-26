194b93a6f285c0776f92a53cdfe56bbe43b74d30
Marcin Gryszkalis <mg@fork.pl>
2020-10-26 23:09:03 +0100

    d06 2015 cleanup

--- 2015/d06.pl
+++ 2015/d06.pl
@@ -10,120 +10,52 @@ my @matrix;
 my $N = 1000;
 
 
-for my $x (0..$N-1)
+
+for my $stage (1..2)
 {
-    for my $y (0..$N-1)
+    #push @matrix, [(0)x$N] for (0..$N-1);
+    for my $x (0..$N-1)
     {
-        $matrix[$x][$y] = 0;
+        for my $y (0..$N-1)
+        {
+            $matrix[$x][$y] = 0;
+        }
     }
-}
 
-
-for (@f)
-{
-    my ($a,$x1,$y1,$x2,$y2) = m/(toggle|turn on|turn off)\s+(\d+),(\d+).*?(\d+),(\d+)/;
-
-    if ($a eq 'toggle')
+    for (@f)
     {
+        my ($a,$x1,$y1,$x2,$y2) = m/(toggle|turn on|turn off)\s+(\d+),(\d+).*?(\d+),(\d+)/;
+
         for my $x ($x1..$x2)
         {
             for my $y ($y1..$y2)
             {
-                $matrix[$x][$y] = ($matrix[$x][$y] + 1) % 2;
+                if ($a eq 'toggle')
+                {
+                    $matrix[$x][$y] = $stage == 1 ? ($matrix[$x][$y] + 1) % 2 : $matrix[$x][$y] + 2;
+                }
+                elsif ($a eq 'turn on')
+                {
+                    $matrix[$x][$y] = $stage == 1 ? 1 : $matrix[$x][$y] + 1;
+                }
+                else # off
+                {
+                    $matrix[$x][$y] = $matrix[$x][$y] - 1; # for stage 1 it never exceeds 1
+                    $matrix[$x][$y] = 0 if $matrix[$x][$y] < 0;
+                }
             }
         }
     }
-    elsif ($a eq 'turn on')
+
+    my $i = 0;
+    for my $x (0..$N-1)
     {
-        for my $x ($x1..$x2)
+        for my $y (0..$N-1)
         {
-            for my $y ($y1..$y2)
-            {
-                $matrix[$x][$y] = 1;
-            }
+            $i += $matrix[$x][$y];
         }
     }
-    else # off
-    {
-        for my $x ($x1..$x2)
-        {
-            for my $y ($y1..$y2)
-            {
-                $matrix[$x][$y] = 0;
-            }
-        }
 
-    }
+    print "stage $stage: $i\n";
+
 }
-
-
-my $i = 0;
-for my $x (0..$N-1)
-{
-    for my $y (0..$N-1)
-    {
-        $i++ if $matrix[$x][$y];
-    }
-}
-
-print "stage 1: $i\n";
-
-
-for my $x (0..$N-1)
-{
-    for my $y (0..$N-1)
-    {
-        $matrix[$x][$y] = 0;
-    }
-}
-
-
-for (@f)
-{
-    my ($a,$x1,$y1,$x2,$y2) = m/(toggle|turn on|turn off)\s+(\d+),(\d+).*?(\d+),(\d+)/;
-
-    if ($a eq 'toggle')
-    {
-        for my $x ($x1..$x2)
-        {
-            for my $y ($y1..$y2)
-            {
-                $matrix[$x][$y] += 2;
-            }
-        }
-    }
-    elsif ($a eq 'turn on')
-    {
-        for my $x ($x1..$x2)
-        {
-            for my $y ($y1..$y2)
-            {
-                $matrix[$x][$y]++;
-            }
-        }
-    }
-    else # off
-    {
-        for my $x ($x1..$x2)
-        {
-            for my $y ($y1..$y2)
-            {
-                $matrix[$x][$y]-- if $matrix[$x][$y] > 0;
-            }
-        }
-
-    }
-}
-
-
-my $i = 0;
-for my $x (0..$N-1)
-{
-    for my $y (0..$N-1)
-    {
-        $i += $matrix[$x][$y];
-    }
-}
-
-print "stage 2: $i\n";
-
