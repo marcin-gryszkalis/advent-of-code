f4aae3dffedd0b9cb49b63db05e93ba418bbe6c9
Marcin Gryszkalis <mg@fork.pl>
2020-11-12 01:08:21 +0100

    double linked list for stage 2

--- 2018/d09.pl
+++ 2018/d09.pl
@@ -7,47 +7,50 @@ use File::Slurp;
 use Algorithm::Combinatorics qw(combinations permutations);
 use Digest::MD5 qw(md5 md5_hex md5_base64);
 
-sub submod($$$)
-{
-    my ($a, $b, $m) = @_;
-    return ($a % $m - $b % $m) % $m;
-}
+# use List::DoubleLinked; -- not circular, other problems
 
 # input:
 # 464 players; last marble is worth 71730 points
 my $PLR = 464;
-my $LASTM = 71730 * 100;
 
-
-my @h = (0);
-my $cnt = 1;
-
-my $score;
-
-my $i = 1; # marble
-my $p = 0; # player
-my $cm = 0; # current marble pos
-while ($i <= $LASTM)
+for my $stage (1..2)
 {
-    if ($i % 23 == 0)
-    {
-        my $mm7 = ($cm - 7) % $cnt;
-        my $bonus = $i + $h[$mm7];
-        $score->{$p+1} += $bonus;
-        $cm = $mm7;
-        splice(@h, $cm, 1);
-        $cnt--;
-    }
-    else # standard case
-    {
-        $cm = (($cm + 1) % $cnt) + 1; # to handle adding behind the end
-        splice(@h, $cm, 0, $i);
-        $cnt++;
-    }
+    my $LASTM = 71730;
+    $LASTM *= 100 if $stage == 2;
 
-    $i++;
-    $p = ($p + 1) % $PLR;
+    my $score = {};
 
-    printf("%8d max=%d\n", $i, max(values(%$score))) if rand() < 0.001;
+    my $i = 1; # marble
+    my $p = 0; # player
+
+    # current marble - simple circular double linked list
+    my $cm = { v => 0, prv => undef, nxt => undef };
+    $cm->{prv} = $cm;
+    $cm->{nxt} = $cm;
+
+    while ($i <= $LASTM)
+    {
+        if ($i % 23 == 0)
+        {
+            $score->{$p} += $i;
+            for (1..7) { $cm = $cm->{prv} }
+            $score->{$p} += $cm->{v};
+            $cm->{prv}{nxt} = $cm->{nxt};
+            $cm->{nxt}{prv} = $cm->{prv};
+            $cm = $cm->{nxt};
+        }
+        else # standard case
+        {
+            $cm = $cm->{nxt};
+            $cm = { v => $i, prv => $cm, nxt => $cm->{nxt} };
+            $cm->{prv}{nxt} = $cm;
+            $cm->{nxt}{prv} = $cm;
+        }
+
+        $i++;
+        $p = ($p + 1) % $PLR;
+
+        # printf("%8d max=%d\n", $i, max(values(%$score))) if rand() < 0.0001;
+    }
+    printf("stage $stage: %d\n", max(values(%$score)));
 }
-printf("stage 1: %d\n", max(values(%$score)));
