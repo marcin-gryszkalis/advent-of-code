d1b845dabc6b2ce218cda414fb7c8433f97bc8d6
Marcin Gryszkalis <mg@fork.pl>
2021-12-02 07:36:09 +0100

    cleanup

--- 2021/d02.pl
+++ 2021/d02.pl
@@ -1,41 +1,18 @@
  #!/usr/bin/perl
 use warnings;
 use strict;
-use Data::Dumper;
-use List::Util qw/min max first sum product all any/;
-use List::MoreUtils qw(uniq);
-use File::Slurp;
-use Algorithm::Combinatorics qw(combinations permutations variations);
-use Clone qw/clone/;
 
-my @f = read_file(\*STDIN);
-@f = map { chomp; $_ } @f;
+my $h = 0;
+my $d = 0;
+my $a = 0;
 
-for my $stage (1..2)
+while (<>)
 {
-    my $h = 0;
-    my $d = 0;
-    my $a = 0;
-
-    if ($stage == 1)
-    {
-        for $_ (@f)
-        {
-            $h += $1 if /forward (\d+)/;
-            $d += $1 if /down (\d+)/;
-            $d -= $1 if /up (\d+)/;
-        }
-    }
-    else
-    {
-        for $_ (@f)
-        {
-            $h += $1 if /forward (\d+)/;
-            $d += $a * $1 if /forward (\d+)/;
-            $a += $1 if /down (\d+)/;
-            $a -= $1 if /up (\d+)/;
-        }
-    }
-
-    printf "Stage %d: %s\n", $stage, $h * $d;
+    $h += $1 if /forward (\d+)/;
+    $d += $a * $1 if /forward (\d+)/;
+    $a += $1 if /down (\d+)/;
+    $a -= $1 if /up (\d+)/;
 }
+
+printf "Stage 1: %s\n", $h * $a;
+printf "Stage 2: %s\n", $h * $d;
