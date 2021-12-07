5c8118141a245de76695ec674498515d19366bcf
Marcin Gryszkalis <mg@fork.pl>
2021-12-07 06:43:50 +0100

    d07 2021 cleanup

--- 2021/d07.pl
+++ 2021/d07.pl
@@ -4,5 +4,5 @@ use strict;
 use List::Util qw/min max first sum product all any uniq head tail/;
 
 my @f = (<> =~ m/(\d+)/g);
-printf "Stage 1: %s\n", min(map { my $p = $_; sum(map { abs($_ - $p) } @f) } (0..max(@f)));;
-printf "Stage 2: %s\n", min(map { my $p = $_; sum(map { abs($_ - $p) * (abs($_ - $p) + 1) / 2 } @f) } (0..max(@f)));;
+printf "Stage 1: %s\n", min(map { my $p = $_; sum(map { abs($_ - $p) } @f) } (0..max(@f)));
+printf "Stage 2: %s\n", min(map { my $p = $_; sum(map { abs($_ - $p) * (abs($_ - $p) + 1) / 2 } @f) } (0..max(@f)));
