2c65edb731b8c2c0b0a2315ba530e3e5f4e10132
Marcin Gryszkalis <mg@fork.pl>
2021-12-14 07:11:47 +0100

    d14 2021

---
+++ 2021/d14.pl
@@ -0,0 +1,52 @@
+ #!/usr/bin/perl
+use warnings;
+use strict;
+use Data::Dumper;
+use List::Util qw/min max first sum product all any uniq head tail/;
+use File::Slurp;
+use Algorithm::Combinatorics qw(combinations permutations variations);
+use Clone qw/clone/;
+
+my @f = read_file(\*STDIN, chomp => 1);
+my $s = shift(@f);
+shift(@f);
+
+my %m;
+for (@f)
+{
+    /(\S+) -> (\S+)/;
+    $m{$1} = $2;
+}
+
+my $p0;
+for my $i (0..length($s)-2)
+{
+    $p0->{substr($s,$i,2)}++;
+}
+
+for my $step (1..40)
+{
+    my $p = ();
+    for my $h (keys %$p0)
+    {
+        if (exists $m{$h})
+        {
+            my ($a,$b) = split//, $h;
+            $p->{$a.$m{$h}} += $p0->{$h};
+            $p->{$m{$h}.$b} += $p0->{$h};
+        }
+    }
+
+    $p0 = $p;
+
+    my %c = ();
+    for my $a (keys %$p0)
+    {
+        $c{substr($a,0,1)} += $p0->{$a};
+    }
+    $c{substr($s,-1,1)}++; # last letter of string is not first in any pair
+
+    printf "$step %s\n", max(values %c) - min(values %c);
+}
+
+
