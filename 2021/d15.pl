bd2427933fc36710393a363159b10b847fb8750a
Marcin Gryszkalis <mg@fork.pl>
2021-12-15 08:25:47 +0100

    d15 2021 cleanup

--- 2021/d15.pl
+++ 2021/d15.pl
@@ -9,48 +9,35 @@ use Clone qw/clone/;
 use Graph::Directed;
 no warnings 'recursion';
 
-my @f = read_file(\*STDIN, chomp => 1);
+my @f = map { [split//] } read_file(\*STDIN, chomp => 1);
 
 my $g = Graph::Directed->new;
 
 my $wy = scalar(@f);
-my $wx = length($f[0]);
+my $wx = scalar(@{$f[0]});
 
-my $awy = 5 * $wy;
-my $awx = 5 * $wx;
+my $maxy = 5 * $wy - 1;
+my $maxx = 5 * $wx - 1;
 
-my $y = 0;
-for (@f)
+for my $y (0..$maxy)
 {
-    my $x = 0;
-    for my $e (split//)
+    for my $x (0..$maxx)
     {
-        for my $dy (0..4)
-        {
-            for my $dx (0..4)
-            {
-                my $ax = $x + $wx*$dx;
-                my $ay = $y + $wy*$dy;
+        my $e = $f[$y % $wy]->[$x % $wx];
+        my $ae = (($e - 1) + int($x / $wx) + int($y / $wy)) % 9 + 1;
 
-                my $ae = (($e - 1) + $dx + $dy) % 9 + 1;
-
-                $g->add_weighted_edge(($ax-1).$;.$ay, "$ax$;$ay", $ae) if $ax > 0;
-                $g->add_weighted_edge($ax.$;.($ay-1), "$ax$;$ay", $ae) if $ay > 0;
-                $g->add_weighted_edge(($ax+1).$;.$ay, "$ax$;$ay", $ae) if $ax < $awx-1;
-                $g->add_weighted_edge($ax.$;.($ay+1), "$ax$;$ay", $ae) if $ay < $awy-1;
-            }
-        }
-
-        $x++;
+        $g->add_weighted_edge(($x-1).$;.$y, "$x$;$y", $ae) if $x > 0;
+        $g->add_weighted_edge($x.$;.($y-1), "$x$;$y", $ae) if $y > 0;
+        $g->add_weighted_edge(($x+1).$;.$y, "$x$;$y", $ae) if $x < $maxx;
+        $g->add_weighted_edge($x.$;.($y+1), "$x$;$y", $ae) if $y < $maxy;
     }
-    $y++;
 }
 
 print "vertices: ".scalar($g->vertices)."\n";
 print "edges: ".scalar($g->edges)."\n";
 
 my @p1 = $g->SP_Dijkstra("0$;0", ($wx-1).$;.($wy-1));
-my @p2 = $g->SP_Dijkstra("0$;0", ($awx-1).$;.($awy-1));
+my @p2 = $g->SP_Dijkstra("0$;0", "$maxx$;$maxy");
 
 printf "Stage 1: %s\n", sum(map { $g->get_edge_weight($p1[$_], $p1[$_+1]) } (0..$#p1-1));
 printf "Stage 2: %s\n", sum(map { $g->get_edge_weight($p2[$_], $p2[$_+1]) } (0..$#p2-1));
