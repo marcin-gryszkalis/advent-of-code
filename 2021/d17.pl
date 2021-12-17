1bdfcdf8550f80e0b5caf5245b60126dd297e61b
Marcin Gryszkalis <mg@fork.pl>
2021-12-17 09:31:10 +0100

    d17 2021 cleanup and justification of bounds

--- 2021/d17.pl
+++ 2021/d17.pl
@@ -7,26 +7,22 @@ use File::Slurp;
 use Algorithm::Combinatorics qw(combinations permutations variations);
 use Clone qw/clone/;
 
+$_ = "target area: x=20..30, y=-10..-5"; # test case
 $_ = "target area: x=56..76, y=-162..-134";
-#$_ = "target area: x=20..30, y=-10..-5"; # test case
 
 my ($tx1,$tx2,$ty1,$ty2) = m/[\d-]+/g;
-my $txs = ($tx2 - $tx1 + 1);
-my $tys = ($ty2 - $ty1 + 1);
-
-print "target: ($tx1,$tx2) - ($ty1,$ty2) [$txs,$tys]\n";
 
 my $stage1 = 0;
 my $stage2 = 0;
 
-my $maxx = 0;
-my $maxy = 0;
-for my $xv (1..$tx2)
+# for lower bound:
+# https://www.wolframalpha.com/input/?i=solve+for+n%3A+%28n%5E2+-+n%29%2F2+%3E%3D+x
+for my $xv (int((sqrt(8*$tx1)+1)/2)..$tx2)
 {
     my $ixv = $xv;
-
-    # Originally guessed upper bound like this:
-    # for my $yv ($ty1..$tys*10)
+    
+    # Originally guessed yv upper bound like this:
+    # for my $yv ($ty1..$ty_size*10)
 
     # Proper explanation from DFreiberg@reddit
 
@@ -57,16 +53,14 @@ for my $xv (1..$tx2)
             $xv-- if $xv > 0;
             $yv--;
 
-            $hiy = max($y,$hiy);
+            $hiy = max($y, $hiy);
             if ($x >= $tx1 && $x <= $tx2 && $y >= $ty1 && $y <= $ty2)
             {
                 $stage2++;
                 if ($hiy > $stage1)
                 {
                     $stage1 = $hiy;
-                    $maxy = $iyv ;
-                    $maxx = $ixv ;
-                    print "$maxx, $maxy -> $stage1\n";
+                    print "$ixv, $iyv -> $stage1\n";
                 }
 
                 last;
