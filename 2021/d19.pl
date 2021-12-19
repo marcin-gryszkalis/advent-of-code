3a1a1e7ffe36246dd70785a00ad4b6bfca982400
Marcin Gryszkalis <mg@fork.pl>
2021-12-19 08:26:39 +0100

    d19 2021 cleanup

--- 2021/d19.pl
+++ 2021/d19.pl
@@ -4,9 +4,12 @@ use strict;
 use Data::Dumper;
 use List::Util qw/min max first sum product all any uniq head tail reduce/;
 use File::Slurp;
-use Algorithm::Combinatorics qw(combinations permutations variations);
+use Algorithm::Combinatorics qw(combinations permutations variations variations_with_repetition);
 use Clone qw/clone/;
 use Digest::MD5 qw(md5 md5_hex md5_base64);
+
+
+
 
 my @f = read_file(\*STDIN, chomp => 1);
 
@@ -24,80 +27,45 @@ for (@f)
     push(@{$h->{$sc}}, \@a);
 }
 
-my $maxs = max(keys %$h);
+my $maxs = $sc;
 
-$; = ":";
-
+# this way we have 48 transformations (instead of 24 valid)
+# probably half of them is "mirror"
 my @xform;
-
-my @uq;
-for my $x ((-1,1)) {
-for my $y ((-1,1)) {
-for my $z ((-1,1)) {
-my $itp = permutations([0,1,2]);
-PERM: while (my $p = $itp->next)
+my $itv = variations_with_repetition([-1,1], 3);
+while (my $v = $itv->next)
 {
-    my $q;
-    $q->[0] = $p->[0];
-    $q->[1] = $p->[1];
-    $q->[2] = $p->[2];
-    $q->[3] = $x;
-    $q->[4] = $y;
-    $q->[5] = $z;
-
-    push(@xform, $q);
-
-    print join(" ", @$q)."\n";
-}
-}}}
-
-my $c = $h->{0};
-my @xformuq;
-my %xformh;
-
-for my $x (@xform)
-{
-    my $d;
-    for my $r (@$c)
+    my $itp = permutations([0,1,2]);
+    while (my $p = $itp->next)
     {
-        my @a = (
-            $r->[abs($x->[0])] * ($x->[3]),
-            $r->[abs($x->[1])] * ($x->[4]),
-            $r->[abs($x->[2])] * ($x->[5]),
-            );
-        push @$d, \@a;
+        push(@xform, [@$p,@$v]);
     }
-
-    # print "\n\n".join(" ", @$x)."\n";
-    # my $md5h = md5_hex(Dumper $d);
-    # if (!exists($xformh{$md5h}))
-    # {
-    #     $xformh{$md5h} = 1;
-    #     push(@xformuq, $x);
-    # }
-    #print Dumper $d;
 }
 
+$; = ",";
 my $m; # map
 
-
-for my $r (@$c)
+# scanner no 0
+for my $r (@{$h->{0}})
 {
-    $m->{$r->[0],$r->[1],$r->[2]} = 0;
+    $m->{$r->[0],$r->[1],$r->[2]} = 1;
 }
+
 my %done;
 $done{0} = 1;
 
-#print Dumper $m;exit;
+my %scpos;
+$scpos{0} = [0,0,0];
+
 while (1)
 {
-    BIG: for my $i (0..$maxs)
+    SCANNER: for my $i (1..$maxs)
     {
         next if $done{$i};
 
         my $c = $h->{$i};
 
-        for my $x (@xform)
+        for my $x (@xform) # all transforms
         {
             my $d;
             for my $r (@$c)
@@ -114,8 +82,6 @@ while (1)
             {
                 my @pp = split/$;/,$p;
 
-                # my $ps = $m->{$p}; # which scanner?
-
                 for my $dp (@$d) # end every point on transformed scanner d
                 {
                     my @move = (
@@ -127,19 +93,24 @@ while (1)
                     my $cnt = 0;
                     for my $vp (@$d) # verify others
                     {
-                        $cnt++ if (exists $m->{$vp->[0]+$move[0],$vp->[1]+$move[1],$vp->[2]+$move[2]});
+                        $cnt++ if exists $m->{$vp->[0]+$move[0],$vp->[1]+$move[1],$vp->[2]+$move[2]};
+
+                        if ($cnt == 12)
+                        {
+                            printf "match: $i xform(%s) move(%s)\n",join(" ",@$x),join(" ",@move);
+
+                            for my $r (@$d)
+                            {
+                                $m->{$r->[0]+$move[0],$r->[1]+$move[1],$r->[2]+$move[2]} = 1;
+                            }
+
+                            $scpos{$i} = \@move;
+                            $done{$i} = 1;
+
+                            next SCANNER;
+                        }
                     }
 
-                    if ($cnt >= 12)
-                    {
-                        for my $r (@$d)
-                        {
-                            $m->{$r->[0]+$move[0],$r->[1]+$move[1],$r->[2]+$move[2]} = $i;
-                        }
-printf "match: $i xform(%s) move(%s)\n",join(" ",@$x),join(" ",@move);
-                        $done{$i} = 1;
-                        next BIG;
-                    }
                 }
 
             }
@@ -150,11 +121,18 @@ printf "match: $i xform(%s) move(%s)\n",join(" ",@$x),join(" ",@move);
     last if scalar(keys %done) == $maxs+1;
 }
 
-print Dumper $m;
 $stage1 = scalar(keys %$m);
 
-
-#print Dumper $h;
+my $it = combinations([values(%scpos)], 2);
+while (my $pos = $it->next)
+{
+    $stage2 = max(
+        $stage2,
+        abs($pos->[0]->[0] - $pos->[1]->[0]) +
+        abs($pos->[0]->[1] - $pos->[1]->[1]) +
+        abs($pos->[0]->[2] - $pos->[1]->[2])
+        );
+}
 
 printf "Stage 1: %s\n", $stage1;
 printf "Stage 2: %s\n", $stage2;
