31d88eeade7cd7af43b5f7e618f07187a13024e4
Marcin Gryszkalis <mg@fork.pl>
2021-12-21 12:23:35 +0100

    d21 2021 cleanup

--- 2021/d21.pl
+++ 2021/d21.pl
@@ -7,9 +7,9 @@ use File::Slurp;
 use Algorithm::Combinatorics qw(combinations permutations variations variations_with_repetition);
 use Clone qw/clone/;
 
-my $stage1 = 0;
-my $stage2 = 0;
-
+my $target = 21;
+# $target = 11;
+ 
 my $p1 = 7;
 my $p2 = 10;
 
@@ -42,82 +42,43 @@ my %xu = (
     9 => 1,
     );
 
-my $target = 21;
-$target = 11;
- 
-my $s1 = 0;
-my $s2 = 0;
 
-
-my $w1 = 0;
-my $w2 = 0;
+my @w = (0,0);
 
 sub d
 {
-    my $p = shift; 
-    my $l = shift;
-    my $m = shift;
-    my $u = shift;
-    my $s1 = shift;
-    my $s2 = shift;
-    my $q1 = shift;
-    my $q2 = shift;
+    my $l = shift; # level
+    my $p = $l % 2 ? 0 : 1; # player
 
-    my $x1 = shift;
-    my $x2 = shift;
-
-    my $xx1 = $x1;
-    my $xx2 = $x2;
+    my $m = shift; # sum of 3 trows (move)
+    my $u = shift; # universes so far on the path
+    
+    my $s = shift; # scores so far
+    my $q = shift; # positions
+    my $x = shift; # path as string
 
     if ($l > 0)
     {
-
-        if ($p == 1)
-        {
-            $xx1 = "$x1$m";
-            $xx2 = $x2;
-        }
-        else
-        {
-            $xx1 = $x1;
-            $xx2 = "$x2$m";
-        }
-
-        if ($p == 1)
-        {
-            $q1 = ($q1 + $m) % 10;
-            $s1 += ($q1 + 1);
-            if ($s1 >= $target)
+            $x->[$p] .= $m;
+            $q->[$p] = ($q->[$p] + $m) % 10;
+            $s->[$p] += ($q->[$p] + 1);
+            if ($s->[$p] >= $target)
             {
-                $w1 += $u;
-                print "p1 won (score=$s1) at level($l) with u=$u (total $w1) x1=$xx1 x2=$xx2\n";
+                $w[$p] += $u;
+                print "p".($p+1)." won (score=$s->[$p]) at level($l) with u=$u (total $w[$p]) x1=$x->[0] x2=$x->[1]\n";
                 return;
             }
-        }
-        else
-        {
-            $q2 = ($q2 + $m) % 10;
-            $s2 += ($q2 + 1);
-            if ($s2 >= $target)
-            {
-                $w2 += $u;
-                print "p2 won (score=$s2) at level($l) with u=$u (total $w2) x1=$xx1 x2=$xx2\n";
-                return;
-            }
-        }
     }
 
-    my $qq1 = $q1 + 1;
-    my $qq2 = $q2 + 1;
-#    print "p=$p l=$l m=$m u=$u q:$qq1 $qq2 s:$s1 $s2 \n";
+#   print "p=$p l=$l m=$m u=$u q: $q->[0] $q->[1] s: $s->[0] $s->[1] \n";
 
    for my $i (3..9)
-    {
-        d($p == 1 ? 2 : 1, $l+1, $i, $u * $xu{$i}, $s1, $s2, $q1, $q2, $xx1, $xx2);
-    }
+   {
+       d($l+1, $i, $u * $xu{$i}, clone($s), clone($q), clone($x));
+   }
 }
 
-d(2, 0, -1, 1, 0, 0, $p1, $p2, "", "");
-print "$w1\n$w2\n";
+d(0, 1, 1 , [0, 0], [$p1, $p2], ["", ""]);
+print "$w[0]\n$w[1]\n";
 
-printf "Stage 2: %s\n", max($w1,$w2);
+printf "Stage 2: %s\n", max(@w);
