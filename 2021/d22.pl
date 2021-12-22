379e8f7726864dd836032077555f465f931c01c3
Marcin Gryszkalis <mg@fork.pl>
2021-12-22 12:12:32 +0100

    d22 2021 work in progress

--- 2021/d22.pl
+++ 2021/d22.pl
@@ -14,15 +14,22 @@ my $stage2 = 0;
 
 $; = ",";
 
-my @qq;
+my @qq; # list of non-overlaping cubes
+
+sub cube
+{
+    my $q = shift;
+    return "($q->{x1},$q->{x2})-($q->{y1},$q->{y2})-($q->{z1},$q->{z2})";
+}
 
 FOR: for (@f)
 {
+    last if /xxx/;
     next unless /^(on|off)/;
     my $mode = $1;
     my ($x1,$x2,$y1,$y2,$z1,$z2) = /[\d-]+/g;
 
-    #stage1
+#    stage1
     # next if
     #     $x1 < -50 ||
     #     $x2 > 50 ||
@@ -31,228 +38,284 @@ FOR: for (@f)
     #     $z1 < -50 ||
     #     $z2 > 50 ;
 
-    if ($mode eq 'on')
+    my @nq = ();
+    my @fq = ();
+
+    my %tq;
+    $tq{x1} = $x1;
+    $tq{x2} = $x2;
+    $tq{y1} = $y1;
+    $tq{y2} = $y2;
+    $tq{z1} = $z1;
+    $tq{z2} = $z2;
+
+    printf "START %s\n", cube(\%tq);
+
+    if (scalar(@qq) == 0)
     {
-        my @nq = ();
-        my @fq = ();
+        push(@qq, \%tq);
+        next FOR;
+    }
 
-        my %tq;
-        $tq{x1} = $x1;
-        $tq{x2} = $x2;
-        $tq{y1} = $y1;
-        $tq{y2} = $y2;
-        $tq{z1} = $z1;
-        $tq{z2} = $z2;
+    push(@nq, \%tq);
 
-        if (scalar(@qq) == 0)
-        {
-            push(@qq, \%tq);
-            next FOR;
-        }
-
-        push(@nq, \%tq);
-        NQ: while (1)
-        {
-            my $p = pop(@nq); # p is new one (may come from split)
-            last unless defined $p;
+    NQ: while (1)
+    {
+        my $p = pop(@nq); # p is new one (may come from split)
+        last unless defined $p;
 
 #            printf("nqs(%d)\n", scalar @nq);
-            print "P: $p->{x1},$p->{x2} $p->{y1},$p->{y2} $p->{y1},$p->{y2}\n";
+        printf STDERR "NEW P: %s\n", cube($p);
+        printf STDERR "QQ(%d)\n", scalar(@qq);
 
+        my $psplit = 0; # if p got split
 
-            my $psplit = 0; # if p got split
+        QQ: while (1)
+        {
+            my $q = pop(@qq);
+            last unless defined $q;
 
-            QQ: while (1)
+#            print "Q: $q->{x1},$q->{x2} $q->{y1},$q->{y2} $q->{z1},$q->{z2}\n";
+
+            # no overlap
+            if (
+                ($p->{x2} < $q->{x1} || $p->{x1} > $q->{x2}) ||
+                ($p->{y2} < $q->{y1} || $p->{y1} > $q->{y2}) ||
+                ($p->{z2} < $q->{z1} || $p->{z1} > $q->{z2}))
             {
-                my $q = pop(@qq);
-                last unless defined $q;
+#                printf "No Overlap Q (%s)\n", cube($q);
+                push(@fq, $q);
+                next QQ;
+            }
 
-                print "Q: $q->{x1},$q->{x2} $q->{y1},$q->{y2} $q->{y1},$q->{y2}\n";
-                # no overlap
-                if (
-                    ($p->{x2} < $q->{x1} || $p->{x1} > $q->{x2}) &&
-                    ($p->{y2} < $q->{y1} || $p->{y1} > $q->{y2}) &&
-                    ($p->{z2} < $q->{z1} || $p->{z1} > $q->{z2}))
+            # if ($mode eq 'on') # for off it's just split
+            # {
+            #     # p in q
+            #     if (
+            #         ($p->{x1} >= $q->{x1} && $p->{x2} <= $q->{x2}) &&
+            #         ($p->{y1} >= $q->{y1} && $p->{y2} <= $q->{y2}) &&
+            #         ($p->{z1} >= $q->{z1} && $p->{z2} <= $q->{z2}))
+            #     {
+            #         # p lost in q
+            #         print "P in Q (%s)\n", cube($q);
+            #         push(@fq, $q);
+            #         push(@qq, @fq);
+            #         @fq = ();
+
+            #         next NQ;
+            #     }
+            # }
+
+            # q in p
+            if (
+                ($q->{x1} >= $p->{x1} && $q->{x2} <= $p->{x2}) &&
+                ($q->{y1} >= $p->{y1} && $q->{y2} <= $p->{y2}) &&
+                ($q->{z1} >= $p->{z1} && $q->{z2} <= $p->{z2}))
+            {
+                # q lost in p, but p may overlap with others!
+                # forget about q!
+                printf "Q in P, Lost Q (%s)\n", cube($q);
+                next QQ;
+            }
+
+            # partial overlap
+            $psplit = 1;
+
+            my @sx = (); # split axis
+            my @sy = ();
+            my @sz = ();
+
+            push(@sx, "L$q->{x1}", "R$q->{x2}", "L$p->{x1}", "R$p->{x2}");
+            push(@sy, "L$q->{y1}", "R$q->{y2}", "L$p->{y1}", "R$p->{y2}");
+            push(@sz, "L$q->{z1}", "R$q->{z2}", "L$p->{z1}", "R$p->{z2}");
+
+            @sx = sort @sx; @sx = uniq(@sx);
+            @sy = sort @sy; @sy = uniq(@sy);
+            @sz = sort @sz; @sz = uniq(@sz);
+
+            # pop(@sx); shift(@sx);
+            # pop(@sy); shift(@sy);
+            # pop(@sz); shift(@sz);
+
+# print "sx: ", Dumper \@sx;
+# print "sy: ", Dumper \@sy;
+# print "sz: ", Dumper \@sz;
+            my @sq;
+            push(@sq, $p);
+            push(@sq, $q);
+            my @sqf = ();
+            SQ: while (1)
+            {
+                my $s = pop(@sq);
+                last unless defined $s;
+
+                my $lr;
+                for my $axx (@sx)
                 {
-                    push(@fq, $q);
-                    next QQ;
-                }
-
-                # p in q
-                if (
-                    ($p->{x1} >= $q->{x1} && $p->{x2} <= $q->{x2}) &&
-                    ($p->{y1} >= $q->{y1} && $p->{y2} <= $q->{y2}) &&
-                    ($p->{z1} >= $q->{z1} && $p->{z2} <= $q->{z2}))
-                {
-                    # p lost in q
-                    push(@fq, $q);
-                    next NQ;
-                }
-
-                # q in p
-                if (
-                    ($q->{x1} >= $p->{x1} && $q->{x2} <= $p->{x2}) &&
-                    ($q->{y1} >= $p->{y1} && $q->{y2} <= $p->{y2}) &&
-                    ($q->{z1} >= $p->{z1} && $q->{z2} <= $p->{z2}))
-                {
-                    # q lost in p, but p may overlap with others!
-                    # forget about q!
-                    next QQ;
-                }
-
-                # partial overlap
-                $psplit = 1;
-
-                my %sidex;
-                my %sidey;
-                my %sidez;
-
-                my @sx = (); # split axis
-                my @sy = ();
-                my @sz = ();
-
-                push(@sx, $q->{x1}, $q->{x2}, $p->{x1}, $p->{x2});
-                push(@sy, $q->{y1}, $q->{y2}, $p->{y1}, $p->{y2});
-                push(@sz, $q->{z1}, $q->{z2}, $p->{z1}, $p->{z2});
-
-                $sidex{$q->{x1}} = 'L'; $sidex{$q->{x2}} = 'R';
-                $sidex{$p->{x1}} = 'L'; $sidex{$p->{x2}} = 'R';
-                $sidey{$q->{y1}} = 'L'; $sidey{$q->{y2}} = 'R';
-                $sidey{$p->{y1}} = 'L'; $sidey{$p->{y2}} = 'R';
-                $sidez{$q->{z1}} = 'L'; $sidez{$q->{z2}} = 'R';
-                $sidez{$p->{z1}} = 'L'; $sidez{$p->{z2}} = 'R';
-
-                @sx = sort { $a <=> $b } @sx; @sx = uniq(@sx);
-                @sy = sort { $a <=> $b } @sy; @sy = uniq(@sy);
-                @sz = sort { $a <=> $b } @sz; @sz = uniq(@sz);
-
-                pop(@sx); shift(@sx);
-                pop(@sy); shift(@sy);
-                pop(@sz); shift(@sz);
-
-                my @sq;
-                push(@sq, $p);
-                push(@sq, $q);
-                my @sqf = ();
-                SQ: while (1)
-                {
-                    my $s = pop(@sq);
-                    last unless defined $s;
-
-                    for my $ax (@sx)
+                    die unless $axx =~ /^(.)(.+)/;
+                    $lr = $1; my $ax = $2;
+#                    print "$lr :: $ax\n";
+                    if (
+                        ($ax > $s->{x1} || $ax == $s->{x1} && $lr eq 'R') &&
+                        ($ax < $s->{x2} || $ax == $s->{x2} && $lr eq 'L')
+                        )
                     {
-                        if ($ax > $s->{x1} && $ax < $s->{x2})
+                        my %n1 = %$s;
+                        my %n2 = %$s;
+
+                        $n1{x1} = $s->{x1};
+                        if ($lr eq 'L')
                         {
-                            my %n1 = %$s;
-                            my %n2 = %$s;
-
-                            $n1{x1} = $s->{x1};
-                            if ($sidex{$ax} eq 'L')
-                            {
-                                $n1{x2} = $ax-1;
-                                $n2{x1} = $ax;
-                            }
-                            else
-                            {
-                                $n1{x2} = $ax;
-                                $n2{x1} = $ax+1;
-                            }
-                            $n2{x2} = $s->{x2};
-
-                            unshift(@sq, \%n1);
-                            unshift(@sq, \%n2);
-                            next SQ;
+                            $n1{x2} = $ax-1;
+                            $n2{x1} = $ax;
                         }
-                    }
-
-                    for my $ay (@sy)
-                    {
-                        if ($ay > $s->{y1} && $ay < $s->{y2})
+                        else
                         {
-                            my %n1 = %$s;
-                            my %n2 = %$s;
-                            $n1{y1} = $s->{y1};
-                            if ($sidey{$ay} eq 'L')
-                            {
-                                $n1{y2} = $ay-1;
-                                $n2{y1} = $ay;
-                            }
-                            else
-                            {
-                                $n1{y2} = $ay;
-                                $n2{y1} = $ay+1;
-                            }
-                            $n2{y2} = $s->{y2};
-                            unshift(@sq, \%n1);
-                            unshift(@sq, \%n2);
-                            next SQ;
+                            $n1{x2} = $ax;
+                            $n2{x1} = $ax+1;
                         }
-                    }
+                        $n2{x2} = $s->{x2};
 
-                    for my $az (@sz)
-                    {
-                        if ($az > $s->{z1} && $az < $s->{z2})
-                        {
-                            my %n1 = %$s;
-                            my %n2 = %$s;
-                            $n1{z1} = $s->{z1};
-                            if ($sidez{$az} eq 'L')
-                            {
-                                $n1{z2} = $az-1;
-                                $n2{z1} = $az;
-                            }
-                            else
-                            {
-                                $n1{z2} = $az;
-                                $n2{z1} = $az+1;
-                            }
-                            $n2{z2} = $s->{z2};
-                            unshift(@sq, \%n1);
-                            unshift(@sq, \%n2);
-                            next SQ;
-                        }
+                        unshift(@sq, \%n1);
+                        unshift(@sq, \%n2);
+                        next SQ;
                     }
-
-                    push(@sqf, $s);
                 }
 
-                # remove dupes from @sq
-                my %sqh;
-                for my $s (@sqf)
+                for my $ayy (@sy)
                 {
-                    $sqh{$s->{x1},$s->{x2},$s->{y1},$s->{y2},$s->{z1},$s->{z2}} = 1;
+                    die unless $ayy =~ /^(.)(.+)/;
+                    $lr = $1; my $ay = $2;
+                    if (
+                        ($ay > $s->{y1} || $ay == $s->{y1} && $lr eq 'R') &&
+                        ($ay < $s->{y2} || $ay == $s->{y2} && $lr eq 'L')
+                        )
+                    {
+                        my %n1 = %$s;
+                        my %n2 = %$s;
+                        $n1{y1} = $s->{y1};
+                        if ($lr eq 'L')
+                        {
+                            $n1{y2} = $ay-1;
+                            $n2{y1} = $ay;
+                        }
+                        else
+                        {
+                            $n1{y2} = $ay;
+                            $n2{y1} = $ay+1;
+                        }
+                        $n2{y2} = $s->{y2};
+                        unshift(@sq, \%n1);
+                        unshift(@sq, \%n2);
+                        next SQ;
+                    }
                 }
 
-                @sq = ();
-                for my $k (keys %sqh)
+                for my $azz (@sz)
                 {
-                    my @kk = split(/$;/, $k);
-                    my %s = ();
-                    $s{x1} = $kk[0];
-                    $s{x2} = $kk[1];
-                    $s{y1} = $kk[2];
-                    $s{y2} = $kk[3];
-                    $s{z1} = $kk[4];
-                    $s{z2} = $kk[5];
-                    push(@sq, \%s);
+                    die unless $azz =~ /^(.)(.+)/;
+                    $lr = $1; my $az = $2;
+                    if (
+                        ($az > $s->{z1} || $az == $s->{z1} && $lr eq 'R') &&
+                        ($az < $s->{z2} || $az == $s->{z2} && $lr eq 'L')
+                        )
+                    {
+                        my %n1 = %$s;
+                        my %n2 = %$s;
+                        $n1{z1} = $s->{z1};
+                        if ($lr eq 'L')
+                        {
+                            $n1{z2} = $az-1;
+                            $n2{z1} = $az;
+                        }
+                        else
+                        {
+                            $n1{z2} = $az;
+                            $n2{z1} = $az+1;
+                        }
+                        $n2{z2} = $s->{z2};
+                        unshift(@sq, \%n1);
+                        unshift(@sq, \%n2);
+                        next SQ;
+                    }
                 }
 
+                push(@sqf, $s);
+            }
+
+            # remove dupes from @sq
+            my %sqh;
+            for my $s (@sqf)
+            {
+                $sqh{$s->{x1},$s->{x2},$s->{y1},$s->{y2},$s->{z1},$s->{z2}} = 1;
+            }
+print Dumper \%sqh;
+
+            @sq = ();
+            for my $k (keys %sqh)
+            {
+                my @kk = split(/$;/, $k);
+                my %s = ();
+                $s{x1} = $kk[0];
+                $s{x2} = $kk[1];
+                $s{y1} = $kk[2];
+                $s{y2} = $kk[3];
+                $s{z1} = $kk[4];
+                $s{z2} = $kk[5];
+
+                if ($mode eq 'off') # skip these inside of $p
+                {
+                    if (
+                    ($s{x1} >= $p->{x1} && $s{x2} <= $p->{x2}) &&
+                    ($s{y1} >= $p->{y1} && $s{y2} <= $p->{y2}) &&
+                    ($s{z1} >= $p->{z1} && $s{z2} <= $p->{z2}))
+                    {
+                        printf "S inside P, off: %s\n", cube(\%s);
+                        next;
+
+                    }
+                }
+
+                push(@sq, \%s);
+            }
+
+            printf "SQ LEFT(%d)\n", scalar(@sq);
+
+            if ($mode eq 'on')
+            {
                 unshift(@nq, @sq); # push both split p and split q to @nq
-                last QQ;
+            }
+            else
+            {
+                unshift(@fq, @sq); # save those left alive to @fq
+                next QQ;
+            }
+            last QQ;
 
-            } # qq
+        } # qq
 
-            push(@fq, $p) unless $psplit;
-        } # nq
-        @qq = @fq;
-    }
-    else # off
-    {
+        if ($mode eq 'on' && !$psplit)
+        {
+            push(@fq, $p);
+        }
 
-    }
+        push(@qq, @fq);
+        @fq = ();
+    } # nq
+#    @qq = @fq; # XXX or push?
+    push(@qq, @fq);
 
 }
-print Dumper \@qq;
+#print Dumper \@qq;
+
+for my $q (@qq)
+{
+    printf "sum: %s\n", cube($q);
+    $stage1 +=
+        ($q->{x2} - $q->{x1} + 1) *
+        ($q->{y2} - $q->{y1} + 1) *
+        ($q->{z2} - $q->{z1} + 1);
+}
 
 printf "Stage 1: %s\n", $stage1;
 printf "Stage 2: %s\n", $stage2;
