8510acc591736e7a5114e7ba70f0022c8b0fdc39
Marcin Gryszkalis <mg@fork.pl>
2022-12-03 21:30:57 +0100

    cleanup

--- 2022/d03.pl
+++ 2022/d03.pl
@@ -4,87 +4,42 @@ use strict;
 use File::Slurp;
 use Data::Dumper;
 use List::Util qw/min max first sum product all any uniq head tail reduce/;
-use Algorithm::Combinatorics qw(combinations permutations variations);
-use Clone qw/clone/;
 
 my @f = read_file(\*STDIN, chomp => 1);
 
-my $stage1 = 0;
-my $stage2 = 0;
+my $v = 1;
+my %vmap = map { $_, $v++ } ('a'..'z','A'..'Z');
 
-#my %r = ();
-my @rr;
-for (@f)
+sub common
 {
-    my $l = length($_);
-    my $a = substr($_,0,$l/2);
-    my $b = substr($_,$l/2);
-
-    print "$_ $a $b\n";
-
-
-    my %h = ();
-    my %r = ();
-    for my $x (split//,$a)
+    my %cnt;
+    for (@_)
     {
-        $h{$x} = 1;
+        map { $cnt{$_}++ } uniq split//;
     }
 
-    for my $x (split//,$b)
-    {
-        $r{$x} = 1 if exists $h{$x};
-    }
-#    print(Dumper \%r);
-
-    push(@rr, keys %r);
-
+    return grep { $cnt{$_} == scalar(@_) } keys %cnt;
 }
-    print(Dumper \@rr);
-for my $x (@rr)
+
+for my $stage (1..2)
 {
-    my $pr;
-    if ($x =~ /[a-z]/)
+    my @r = ();
+    if ($stage == 1)
     {
-        $pr = ord($x) - ord('a') + 1;
+        for (@f)
+        {
+            my $l = length;
+            push(@r, common(substr($_, 0, $l/2), substr($_, $l/2)));
+        }
     }
     else
     {
-        $pr = ord($x) - ord('A') + 1 + 26;
-    }
-    print "$x $pr\n";
-            $stage1 += $pr;
-
-}
-printf "Stage 1: %s\n", $stage1;
-
-my $i = 0;
-L: while (1)
-{
-    last unless exists $f[$i];
-    my $a = $f[$i++];
-    my $b = $f[$i++];
-    my $c = $f[$i++];
-
-    for my $x (('a'..'z','A'..'Z'))
-    {
-        if ($a =~ /$x/ && $b =~ /$x/ && $c =~ /$x/)
+        while (1)
         {
-            my $pr;
-            if ($x =~ /[a-z]/)
-            {
-                $pr = ord($x) - ord('a') + 1;
-            }
-            else
-            {
-                $pr = ord($x) - ord('A') + 1 + 26;
-            }
-            print "$x $pr\n";
-            $stage2 += $pr;
-
-            next L;
+            push(@r, common(pop(@f),pop(@f),pop(@f)));
+            last unless @f;
         }
     }
-}
-printf "Stage 2: %s\n", $stage2;
 
-# 948
+    printf "Stage %d: %s\n", $stage, sum(map { $vmap{$_}} @r);
+}
