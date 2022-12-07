d74ed9a32fc90e1a0c4113d98401ea49e7be3477
Marcin Gryszkalis <mg@fork.pl>
2022-12-07 07:27:07 +0100

    cleanup

--- 2022/d07.pl
+++ 2022/d07.pl
@@ -14,111 +14,72 @@ shift @f; # skip cd /
 my $stage1 = 0;
 my $stage2 = 0;
 
-my $d = 'ROOT';
-
 my $tt;
-$tt->{$d}->{SIZE} = 0;
-$tt->{$d}->{FP} = $d;
+$tt->{ROOT}->{SIZE} = 0;
+$tt->{ROOT}->{FP} = 'ROOT'; # full path
+
+my $ft; # flat tree
+
+my $xt = $tt->{ROOT};
+my @xstack;
 
 for (@f)
 {
-    print "d($d)\n";
     if (/\$ cd (\S+)/)
     {
         my $n = $1;
         if ($n eq '..')
         {
-            $d =~ s{/[^/]+$}{};
+            $xt = pop(@xstack);
         }
         else
         {
-            $d .= "/$n";
-
-            my @p = split(m{/}, $d);
-            my $xt = $tt;
-            for my $pe (@p)
+            unless (defined $xt->{$n})
             {
-                unless (defined $xt->{$pe})
-                {
-                    $xt->{$pe}->{SIZE} = 0;
-                    $xt->{$pe}->{FP} = $d;
-                }
-                $xt = $xt->{$pe};
+                $xt->{$n}->{SIZE} = 0;
+                $xt->{$n}->{FP} = $xt->{FP}."/".$n;
+                $ft->{$xt->{$n}->{FP}} = 0;
             }
 
+            push(@xstack, $xt);
+            $xt = $xt->{$n};
         }
-       # $d =~ s{/+}{/}g;
     }
     elsif (/\$ ls/)
     {
-        # nop
         next;
     }
     elsif (/dir (\S+)/)
     {
-        my @p = split(m{/}, $d);
-        my $xt = $tt;
-        for my $pe (@p)
-        {
-            unless (defined $xt->{$pe})
-            {
-                $xt->{$pe}->{SIZE} = 0;
-                $xt->{$pe}->{FP} = $d;
-            }
-            $xt = $xt->{$pe};
-        }
-
+        next;
     }
     elsif (/(\d+) (\S+)/)
     {
         my $xs = $1;
 
-        my @p = split(m{/}, $d);
-        my $xt = $tt;
-        for my $pe (@p)
+        $xt->{SIZE} += $xs;
+        $ft->{$xt->{FP}} += $xs;
+        for my $xxt (@xstack)
         {
-            $xt->{$pe}->{SIZE} += $xs;
-            $xt = $xt->{$pe};
+            $xxt->{SIZE} += $xs;
+            $ft->{$xxt->{FP}} += $xs;
         }
-
     }
     else
     {
         die $_;
     }
-
-# print Dumper $tt;
-
 }
-print Dumper $tt;
 
-my @stack;
-push(@stack, $tt);
-my $ft;
-while (1)
-{
-    my $xt = pop(@stack);
-    last unless defined $xt;
-    for my $k (keys %$xt)
-    {
-        push(@stack, $xt->{$k}) unless $k eq 'SIZE' or $k eq 'FP';
-    }
-
-    next unless exists $xt->{SIZE};
-    $ft->{$xt->{FP}} = $xt->{SIZE};
-
-    $stage1 += $xt->{SIZE} if $xt->{SIZE} < 100000;
-}
+# say Dumper $tt;
 
 my $tofree = 30000000 - (70000000 - $tt->{ROOT}->{SIZE});
+
 for my $k (sort { $ft->{$a} <=> $ft->{$b} } keys %$ft)
 {
-    # say "$k $ft->{$k}";
-    if ($ft->{$k} > $tofree)
-    {
-        $stage2 = $ft->{$k};
-        last;
-    }
+    $stage1 += $ft->{$k} if $ft->{$k} < 100000;
+    $stage2 = $ft->{$k} if $stage2 == 0 && $ft->{$k} > $tofree;
 }
+
 printf "Stage 1: %s\n", $stage1;
 printf "Stage 2: %s\n", $stage2;
