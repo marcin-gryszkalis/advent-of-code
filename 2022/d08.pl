1f3be446c024915e04de18ce5371da62f4899d0c
Marcin Gryszkalis <mg@fork.pl>
2022-12-08 23:30:48 +0100

    cleanup

--- 2022/d08.pl
+++ 2022/d08.pl
@@ -7,6 +7,7 @@ use Data::Dumper;
 use List::Util qw/min max first sum product all any uniq head tail reduce/;
 use Algorithm::Combinatorics qw(combinations permutations variations);
 use Clone qw/clone/;
+$" = ":"; # set separator for multidimensional hash
 
 my @f = read_file(\*STDIN, chomp => 1);
 
@@ -31,113 +32,75 @@ my $w = length($f[0]);
 my $maxy = $h - 1;
 my $maxx = $w - 1;
 
-
 $stage1 = ($maxx+1)*($maxy+1);
 for my $y (1..$maxy-1)
 {
-    S: for my $x (1..$maxx-1)
+    for my $x (1..$maxx-1)
     {
         my $v = $t->{$x,$y};
 
         my $nv = 0;
-        for my $tx (0..$x-1)
-        {
-            if ($t->{$tx,$y} >= $v)
-            {
-                $nv++;
-                last;
-            }
-        }
-        for my $tx ($x+1..$maxy)
-        {
-            if ($t->{$tx,$y} >= $v)
-            {
-                $nv++;
-                last;
-            }
-        }
-
-        for my $ty (0..$y-1)
-        {
-            if ($t->{$x,$ty} >= $v)
-            {
-                $nv++;
-                last;
-            }
-        }
-
-        for my $ty ($y+1..$maxy)
-        {
-            if ($t->{$x,$ty} >= $v)
-            {
-                $nv++;
-                last;
-            }
-        }
+        for my $tx (0..$x-1)     { if ($t->{$tx,$y} >= $v) { $nv++; last; }}
+        for my $tx ($x+1..$maxy) { if ($t->{$tx,$y} >= $v) { $nv++; last; }}
+        for my $ty (0..$y-1)     { if ($t->{$x,$ty} >= $v) { $nv++; last; }}
+        for my $ty ($y+1..$maxy) { if ($t->{$x,$ty} >= $v) { $nv++; last; }}
 
         $stage1-- if $nv == 4;
     }
-
 }
-
 
 my $best = 0;
 for my $y (1..$maxy-1)
 {
-    S: for my $x (1..$maxx-1)
+    for my $x (1..$maxx-1)
     {
         my $v = $t->{$x,$y};
 
         my $nv = 0;
+        my @sc = ();
+        my $sci = 0;
 
         my $tx = $x-1;
-        my $sc1 = 0;
         while (1)
         {
-            $sc1++;
-            last if ($t->{$tx,$y} >= $v);
-            last if $tx == 0;
+            $sc[$sci]++;
+            last if $tx == 0 || $t->{$tx,$y} >= $v;
             $tx--;
         }
+        $sci++;
 
         $tx = $x+1;
-        my $sc2 = 0;
         while (1)
         {
-            $sc2++;
-            last if ($t->{$tx,$y} >= $v);
-            last if $tx == $maxx;
+            $sc[$sci]++;
+            last if $tx == $maxx || $t->{$tx,$y} >= $v;
             $tx++;
         }
+        $sci++;
 
         my $ty = $y-1;
-        my $sc3 = 0;
         while (1)
         {
-            $sc3++;
-            last if ($t->{$x,$ty} >= $v);
-            last if $ty == 0;
+            $sc[$sci]++;
+            last if $ty == 0 || $t->{$x,$ty} >= $v;
             $ty--;
         }
+        $sci++;
 
         $ty = $y+1;
-        my $sc4 = 0;
         while (1)
         {
-            $sc4++;
-            last if ($t->{$x,$ty} >= $v);
-            last if $ty == $maxy;
+            $sc[$sci]++;
+            last if $ty == $maxy || $t->{$x,$ty} >= $v;
             $ty++;
         }
 
-        my $mm = $sc1*$sc2*$sc3*$sc4;
-        if ($mm > $best)
-        {
-            $best = $mm;
-
-        }
+        my $mm = product @sc;
+        $stage2 = $mm if $mm > $stage2;
+#        print "$sc1 $sc2 $sc3 $sc4 = $mm ($stage2)\n";
     }
 
 }
+
 printf "Stage 1: %s\n", $stage1;
-printf "Stage 2: %s\n", $best;
+printf "Stage 2: %s\n", $stage2;
