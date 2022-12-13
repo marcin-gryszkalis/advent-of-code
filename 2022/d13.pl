80fa9fd6869aea52588a3421ea61ef35811ddb36
Marcin Gryszkalis <mg@fork.pl>
2022-12-13 07:09:48 +0100

    cleanup

--- 2022/d13.pl
+++ 2022/d13.pl
@@ -7,115 +7,92 @@ use List::Util qw/min max first sum product all any uniq head tail reduce/;
 use Algorithm::Combinatorics qw(combinations permutations variations);
 use Clone qw/clone/;
 
-my @f = read_file(\*STDIN, chomp => 1);
+my @ft = read_file(\*STDIN, chomp => 1);
+my @f;
+for my $a (@ft)
+{
+    next unless $a =~ /\[/;
+    eval "\$a = $a";
+    push(@f,$a);
+}
 
 my $stage1 = 0;
-my $stage2 = 0;
-
 
 sub compare
 {
     my ($l,$r) = @_;
 
-    my $li = 0;
-    my $ri = 0;
+    my $i = 0;
     while (1)
     {
-        if (!exists $l->[$li] && exists $r->[$ri])
+        if (!exists $l->[$i] && exists $r->[$i])
         {
- #           print "left out $li\n";
             return 1;
         }
-        elsif (exists $l->[$li] && !exists $r->[$ri])
+        elsif (exists $l->[$i] && !exists $r->[$i])
         {
-            #print "right out $ri\n";
             return -1;
         }
-        elsif (!exists $l->[$li] && !exists $r->[$ri]) # eol
+        elsif (!exists $l->[$i] && !exists $r->[$i]) # eol
         {
-            #print "both out $li $ri\n";
             return 0;
         }
 
         my $res = 0;
-        if (ref $l->[$li] && !ref $r->[$ri])
+        if (ref $l->[$i] && !ref $r->[$i])
         {
-            $res = compare($l->[$li], [$r->[$ri]]);
+            $res = compare($l->[$i], [$r->[$i]]);
         }
-        elsif (!ref $l->[$li] && ref $r->[$ri])
+        elsif (!ref $l->[$i] && ref $r->[$i])
         {
-            $res = compare([$l->[$li]], $r->[$ri]);
+            $res = compare([$l->[$i]], $r->[$i]);
         }
-        elsif (ref $l->[$li] && ref $r->[$ri])
+        elsif (ref $l->[$i] && ref $r->[$i])
         {
-            $res = compare($l->[$li], $r->[$ri]);
+            $res = compare($l->[$i], $r->[$i]);
         }
-        else # compare ints
+        else
         {
-            # comapre ints
-            if ($l->[$li] < $r->[$ri])
+            if ($l->[$i] < $r->[$i])
             {
                 $res = 1;
             }
-            elsif ($l->[$li] > $r->[$ri])
+            elsif ($l->[$i] > $r->[$i])
             {
                 $res = -1;
             }
         }
 
-        return $res if ($res != 0);
-
-        # else - continue
-
-        $li++;
-        $ri++;
+        return $res if $res != 0;
+        $i++;
     }
 
 }
 
-my @ff;
-for my $a (@f)
-{
-    next unless $a =~ /\[/;
-    eval "\$a = $a";
-    push(@ff,$a);
-}
-
 my $i = 0;
-my $pi = 1;
-while (exists $ff[$i])
+while (exists $f[$i])
 {
-    my $l = $ff[$i];
-    my $r = $ff[$i+1];
-
-    my $res = compare($l, $r);
-    $stage1 += $pi if $res == 1;
-    print "out $pi $res $stage1\n";
-    $i+=2;
-    $pi++;
+    $stage1 += $i/2 + 1 if compare($f[$i], $f[$i+1]) == 1;
+    $i += 2;
 }
 
-
-push(@ff,[[2]]);
-push(@ff,[[6]]);
-#print Dumper \@ff; exit;
+push(@f,[[2]],[[6]]);
 
 my $d1 = 0;
 my $d2 = 0;
-my @g = sort { compare($b,$a) } @ff;
 
 $i = 1;
-for my $a (@g)
+for my $a (sort { compare($b,$a) } @f)
 {
     $_ = Dumper $a;
     s/.*=//;
-    s/\s+//g;
-    s/;//g;
-    print "$_\n";
+    s/[\s;]+//g;
+
     $d1 = $i if $_ eq '[[2]]';
     $d2 = $i if $_ eq '[[6]]';
+
     $i++;
 }
-$stage2 = $d1 * $d2;
+
 printf "Stage 1: %s\n", $stage1;
-printf "Stage 2: $d1 x $d2 = %s\n", $stage2;
+printf "Stage 2: $d1 x $d2 = %s\n", $d1 * $d2;
