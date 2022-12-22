6a34aaf8460c3d2771ec4ac61eff6fd1e6215699
Marcin Gryszkalis <mg@fork.pl>
2022-12-22 19:04:12 +0100

    day 22 2022 - separate stage 1

---
+++ 2022/d22stage1.pl
@@ -0,0 +1,92 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use feature qw/state signatures say multidimensional/;
+use File::Slurp;
+use Data::Dumper;
+use List::Util qw/min max first sum product all any uniq head tail reduce/;
+use Algorithm::Combinatorics qw(combinations permutations variations);
+use Clone qw/clone/;
+$; = ',';
+
+my @f =  read_file(\*STDIN, chomp => 1);
+my $tx = pop @f;
+pop @f; # empty line
+
+my $maxy = scalar(@f) - 1;
+my $maxx = max(map { length $_ } @f) - 1;
+
+my ($sx,$sy) = (-1,0); # start
+my $m;
+for my $y (0..$maxy)
+{
+    my @r = split//,$f[$y];
+    for my $x (0..$maxx)
+    {
+        next if !defined $r[$x] || $r[$x] eq ' ';
+        $m->{$x,$y} = $r[$x];
+        $sx = $x if $sx == -1 && $r[$x] eq '.';
+    }
+}
+
+my @t = $tx =~ m/(\d+|[LR])/g;
+
+my @mov = ([1,0], [0,1],[-1,0], [0,-1]);
+
+my @g = qw/> V < ^/;
+
+my $x = $sx;
+my $y = $sy;
+my $dir = 0;
+
+$m->{$x,$y} = $g[$dir];
+for my $op (@t)
+{
+#    print "$x, $y [$dir]: $op\n";
+    if ($op =~ /\d/)
+    {
+        my ($px, $py) = ($x, $y);
+
+        while (1)
+        {
+            $x += $mov[$dir][0];
+            $y += $mov[$dir][1];
+
+            $x = 0 if $x > $maxx;
+            $y = 0 if $y > $maxy;
+            $x = $maxx if $x < 0;
+            $y = $maxy if $y < 0;
+
+            next unless exists $m->{$x,$y};
+
+            if ($m->{$x,$y} eq '#')
+            {
+                $x = $px;
+                $y = $py;
+                last;
+            }
+
+            $m->{$x,$y} = $g[$dir];
+
+            ($px, $py) = ($x, $y);
+
+            $op--;
+            last if $op == 0;
+        }
+    }
+    else
+    {
+        $dir = ($dir + ($op eq 'R' ? 1 : -1)) % 4;
+    }
+}
+
+for my $y (0..$maxy)
+{
+    for my $x (0..$maxx)
+    {
+        print exists $m->{$x,$y} ? $m->{$x,$y} : ' ';
+    }
+    print "\n";
+}
+
+printf "Stage 1: %s\n", ($x + 1) * 4 + ($y + 1) * 1000 + $dir;
