ec756ae4a6a09dee6a2ffd87a41ab9895fd3d980
Marcin Gryszkalis <mg@fork.pl>
2022-12-25 07:42:24 +0100

    day 25 2022

---
+++ 2022/d25.pl
@@ -0,0 +1,70 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use feature qw/state signatures say multidimensional/;
+use File::Slurp;
+use Data::Dumper;
+use List::Util qw/min max first sum product all any uniq head tail reduce/;
+use Algorithm::Combinatorics qw(combinations permutations variations);
+use Clone qw/clone/;
+$; = ',';
+
+my @f = read_file(\*STDIN, chomp => 1);
+
+my %snafudigits = qw/
+2 2
+1 1
+0 0
+- -1
+= -2
+/;
+
+sub snafu2dec($a)
+{
+    my $i = 0;
+    return sum(map { (5 ** $i++) * $snafudigits{$_} } reverse split//, $a);
+}
+
+sub dec2pent($n)
+{
+   my $o = "";
+   while ($n)
+   {
+      my $r = $n % 5;
+      $o = $r.$o;
+      $n = ($n-$r)/5;
+   }
+   return $o;
+}
+
+my %xmap = qw/
+0 0
+1 1
+2 2
+3 =
+4 -
+5 0
+/;
+
+sub dec2snafu($a)
+{
+    $a = dec2pent($a);
+    my @x = reverse(split(//, $a));
+
+    my @out = @x;
+    my $k = 0;
+    while (defined $x[$k])
+    {
+        $out[$k] = $xmap{$x[$k]};
+        $x[$k+1]++ if $x[$k] > 2;
+        $k++;
+    }
+
+    return join("", reverse @out)
+}
+
+my $sum = sum(map { snafu2dec($_) } @f);
+
+printf "Stage 1: %s (%s)\n", dec2snafu($sum), $sum;
+print "\n";
+
