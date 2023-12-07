d2e236280486cefb4fa8d5c81dc7d3e29642165c
Marcin Gryszkalis <mg@fork.pl>
2023-12-07 12:00:30 +0100

    cleanup

--- 2023/d07.pl
+++ 2023/d07.pl
@@ -5,16 +5,17 @@ use feature qw/state signatures say multidimensional/;
 use File::Slurp;
 use Data::Dumper;
 use List::Util qw/min max first sum product all any uniq head tail reduce pairs zip mesh/;
+use List::MoreUtils qw/firstidx frequency mode/;
 use POSIX qw/ceil floor/;
-use Algorithm::Combinatorics qw(combinations permutations variations);
+use Algorithm::Combinatorics qw/combinations permutations variations/;
 use Clone qw/clone/;
 $; = ',';
 
 my @f = read_file(\*STDIN, chomp => 1);
 
-my @strengths;
-$strengths[1] = [ qw/2 3 4 5 6 7 8 9 T J Q K A/ ];
-$strengths[2] = [ qw/J 2 3 4 5 6 7 8 9 T Q K A/ ];
+my @strengths = qw/23456789TJQKA J23456789TQKA/;
+my $i = 0;
+my %score = map { $_ => $i++ } qw/11111 1112 122 113 23 14 5/;
 
 my @hands = ();
 my %bids = ();
@@ -28,8 +29,8 @@ for (@f)
 
 for my $stage (1..2)
 {
-    my $i = 0;
-    my %strength = map { $_ => $i++ } @{$strengths[$stage]};
+    $i = 0;
+    my %strength = map { $_ => $i++ } split//, $strengths[$stage-1];
 
     my %figures = ();
 
@@ -38,46 +39,13 @@ for my $stage (1..2)
         my $hj = $h;
         if ($stage == 2)
         {
-            if ($h ne 'JJJJJ') # special case
-            {
-                my %counts;
-                for my $c (split(//,$h)) { $counts{$c}++ }
-
-                my $hnoj = ($h =~ s/J//gr);
-                my $mostfreq = (sort( { $counts{$b} <=> $counts{$a} } sort(split(//,$hnoj))))[0];
-                $hj =~ s/J/$mostfreq/g;
-            }
+            my @modes = mode(grep { /[^J]/ } split(//,$h));
+            my $mostfreq = $modes[1] // 'J'; # modes[0] is the number of occurences
+            $hj =~ s/J/$mostfreq/g;
         }
 
-        my $hjs = join("",sort(split(//,$hj)));
-
-        my $figure = 0;
-        if ($hjs =~ /(.)\1\1\1\1/)
-        {
-            $figure = 6;
-        }
-        elsif ($hjs =~ /(.)\1\1\1/) # 4
-        {
-            $figure = 5;
-        }
-        elsif ($hjs =~ /((.)\2\2(.)\3|(.)\4(.)\5\5)/) # full
-        {
-            $figure = 4;
-        }
-        elsif ($hjs =~ /(.)\1\1/) # 3
-        {
-            $figure = 3;
-        }
-        elsif ($hjs =~ /(.)\1.?(.)\2/) # 2p
-        {
-            $figure = 2;
-        }
-        elsif ($hjs =~ /(.)\1/) # 1p
-        {
-            $figure = 1;
-        }
-
-        $figures{$h} = $figure;
+        my %cj = frequency split//,$hj;
+        $figures{$h} = $score{join("", sort values %cj)};
     }
 
     sub hcomp
@@ -85,27 +53,18 @@ for my $stage (1..2)
         my ($a, $b, $fgs, $str) = @_;
         return $fgs->{$a} <=> $fgs->{$b} if $fgs->{$a} != $fgs->{$b};
 
-        my @aa = split//,$a;
-        my @bb = split//,$b;
-
-        for my $ax (@aa)
+        for (zip([split//,$a],[split//,$b]))
         {
-            my $bx = shift(@bb);
+            my ($ax,$bx) = @$_;
             next if $ax eq $bx;
             return $str->{$ax} <=> $str->{$bx};
         }
 
-        die "the same ($a,$b)";
+        die "the same ($a,$b)"; # assert uniq-ness
     }
 
-    my @xh = sort({ hcomp($a,$b,\%figures,\%strength) } @hands);
+    my @xh = sort({ hcomp($a, $b, \%figures, \%strength) } @hands);
 
-    my $out = 0;
     $i = 1;
-    for my $h (@xh)
-    {
-        $out += $bids{$h} * $i++;
-    }
-
-    printf "Stage $stage: %s\n", $out;
+    printf "Stage $stage: %s\n", sum map { $bids{$_} * $i++ } @xh;
 }
