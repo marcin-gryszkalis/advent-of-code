c3646d08728eb88294a913d7f161289cf605f432
Marcin Gryszkalis <mg@fork.pl>
2023-12-18 09:40:28 +0100

    cleanup

--- 2023/d18.pl
+++ 2023/d18.pl
@@ -13,9 +13,6 @@ $; = ',';
 
 my @f = read_file(\*STDIN, chomp => 1);
 
-my $x = 0;
-my $y = 0;
-
 my $m = {
 '0' => [1,0],
 '1' => [0,1],
@@ -32,8 +29,7 @@ for my $stage (1..2)
 
     my $t = {};
 
-    my ($minx,$maxx) = (0,0);
-    my ($miny,$maxy) = (0,0);
+    my ($x,$y) = (0,0);
 
     # I or E: internal (3 sides inside) / external (3 sides outside) vertex
     my $intext = {};
@@ -48,26 +44,22 @@ for my $stage (1..2)
         my $d = $stage == 1 ? $mm{$d1} : $d2;
         my $l = $stage == 1 ? $l1 : hex($h5);
 
-        # assume we have internal on the right (below we verify)
+        # assume we have internal on the right (i.e. vertices are clockwise):
         # turn right - external, turn left - internal
-        $intext->{$x,$y} = $d - ($pd+4) %4 == 1 ? 'E' : 'I';
+        $intext->{$x,$y} = "$pd$d" =~ /(01|12|23|30)/ ? 'E' : 'I'; #($d - ($pd+4) %4 == 1) ? 'E' : 'I';
 
         $x += $m->{$d}->[0] * $l;
         $y += $m->{$d}->[1] * $l;
-
-        $minx = min($minx,$x);
-        $maxx = max($maxx,$x);
-        $miny = min($miny,$y);
-        $maxy = max($maxy,$y);
 
         push(@{$t->{$y}}, $x);
 
         $pd = $d;
     }
 
-    $y = first { $a <=> $b } keys %$t;
-    $x = first { $a <=> $b } @{$t->{$y}};
-    die "assumption that interior is on the right side was wrong :)" if $intext->{$x,$y} eq 'E';
+    # check left-most vertex from set of top-most vertices
+    $y = min keys %$t;
+    $x = min @{$t->{$y}};
+    die "assumption that interior is on the right side was wrong :)" unless $intext->{$x,$y} eq 'E';
 
     my %state = ();
 
@@ -76,105 +68,31 @@ for my $stage (1..2)
     {
         if (%state)
         {
-            my $v = 0;
-            my $px = 0;
-            my $i = 0;
-            for my $x (sort { $a <=> $b } keys %state)
-            {
-                if ($i%2 == 0) # Left
-                {
-
-                }
-                else # Right
-                {
-                    $v += ($x - $px + 1);
-
-                    print "$y: $px - $x = $v\n";
-                }
-                $i++;
-                $px = $x;
-            }
-
-            printf "multiply by %d\n", ($y - $py - 1);
-            $total += $v * ($y - $py - 1); # lines between
+            my $v = reduce { $a + $b->[1] - $b->[0] + 1 } (0, pairs sort { $a <=> $b } keys %state);
+            $total += $v * ($y - $py - 1); # lines between changes
         }
         $py = $y;
 
-    #    print Dumper \%state;
         my %nstate = %state;
 
-        if (exists $t->{$y})
+        if (exists $t->{$y}) 
         {
             for my $x (sort { $a <=> $b } @{$t->{$y}})
             {
-                if (exists $state{$x}) # to be removed
-                {
-                    if ($intext->{$x,$y} eq 'I') # internal
-                    {
-                        delete $nstate{$x};
-                    }
-                    else # external
-                    {
-                    }
-                }
-                else # new
-                {
-                    if ($intext->{$x,$y} eq 'I') # internal
-                    {
-                    }
-                    else # external
-                    {
-                        $nstate{$x} = 1;
-
-                    }
-                }
+                delete $nstate{$x} if exists $state{$x} && $intext->{$x,$y} eq 'I';
+                $nstate{$x} = 1 if !exists$state{$x} && $intext->{$x,$y} eq 'E';
             }
         }
 
-        my $v = 0;
-        my $px = 0;
-        my $i = 0;
-        for my $x (sort { $a <=> $b } keys %nstate)
-        {
-            if ($i%2 == 0) # Left
-            {
-
-            }
-            else # Right
-            {
-                $v += ($x - $px + 1);
-
-                print "$y: $px - $x = $v\n";# if ($y % 100000 == 0);
-            }
-            $i++;
-            $px = $x;
-        }
+        my $v = reduce { $a + $b->[1] - $b->[0] + 1 } (0, pairs sort { $a <=> $b } keys %nstate);
         $total += $v;
 
         if (exists $t->{$y})
         {
             for my $x (sort { $a <=> $b } @{$t->{$y}})
             {
-                if (exists $state{$x}) # to be removed
-                {
-                    if ($intext->{$x,$y} eq 'I') # internal
-                    {
-                    }
-                    else # external
-                    {
-                        delete $nstate{$x};
-                    }
-                }
-                else # new
-                {
-                    if ($intext->{$x,$y} eq 'I') # internal
-                    {
-                        $nstate{$x} = 1;
-                    }
-                    else # external
-                    {
-                    }
-                }
+                delete $nstate{$x} if exists $state{$x} && $intext->{$x,$y} eq 'E';
+                $nstate{$x} = 1 if !exists$state{$x} && $intext->{$x,$y} eq 'I';
             }
         }
 
