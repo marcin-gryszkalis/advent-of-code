62f603987e984d576d09f2a6fd10b1df8b56e72d
Marcin Gryszkalis <mg@fork.pl>
2023-12-27 14:56:04 +0100

    cleanup

--- 2023/d25.pl
+++ 2023/d25.pl
@@ -9,17 +9,15 @@ use List::MoreUtils qw/firstidx frequency mode slide minmax minmaxstr/;
 use POSIX qw/ceil floor/;
 use Algorithm::Combinatorics qw/combinations permutations variations/;
 use Clone qw/clone/;
+use Graph::Undirected;
 $; = ',';
 
-use Graph::Undirected;
-my $g = Graph::Undirected->new(); # An undirected graph.
-
+# it's possible to do this for all vertex pairs (~1000^2) but usually gives proper answer after 1000-2000 random selections
 my $STABLE_TOP_LIMIT = 500;
 
+my $g = Graph::Undirected->new();
 my @f = read_file(\*STDIN, chomp => 1);
 
-my $t;
-my $c;
 my $cnt;
 for (@f)
 {
@@ -46,37 +44,23 @@ while ($stable < $STABLE_TOP_LIMIT)
 
     my @path = $g->SP_Dijkstra($v, $w);
 
-    for my $vi (1..$#path)
-    {
-        my ($p1,$p2) = minmaxstr($path[$vi-1],$path[$vi]);
-        $cnt{"$p1:$p2"}++;
-    }
+    slide { $cnt{join(":",minmaxstr($a,$b))}++ } @path;
 
     my @toplong = head(6, sort { $cnt{$b} <=> $cnt{$a} } keys %cnt);
     @top = head(3, @toplong);
     printf("%5d ($stable): %s\n", $ii, join(", ", map { "$_ ($cnt{$_})" } @toplong));
-   
-    my $tops = join(",", sort(@top)); # sort because order in @top doesn't matter 
-    if ($tops ne $topsp)
-    {
-        $stable = 0;
-        say "-" x 30;
-    }
 
+    my $tops = join(",", sort(@top)); # sort because order in @top doesn't matter
+    $stable = 0 if $tops ne $topsp;
     $stable++;
     $topsp = $tops;
-    
 }
 
-
-for my $e (@top)
+for (@top)
 {
-    my ($v,$w) = split/:/, $e; 
+    my ($v,$w) = split/:/;
     $g->delete_edge($v,$w);
-#   $g->delete_edge(split/:/);
 }
-
 
 my @cc = $g->connected_components();
-printf "%s = %d\n", join(" x ", map { scalar(@$_) } @cc), product(map { scalar(@$_) } @cc);
-
+printf "Stage1: %s = %d\n", join(" x ", map { scalar(@$_) } @cc), product(map { scalar(@$_) } @cc);
