82d82a46a2f43417016cedee74c45326009aa09a
Marcin Gryszkalis <mg@fork.pl>
2024-12-02 00:00:18 +0100

    2024, day 01

---
+++ 2024/d01.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use feature qw/state signatures say multidimensional/;
+use File::Slurp;
+use Data::Dumper;
+use List::Util qw/min max first sum sum0 product all any uniq head tail reduce pairs pairkeys pairvalues zip mesh/;
+use List::MoreUtils qw/firstidx frequency mode pairwise slide minmax minmaxstr/;
+use Algorithm::Combinatorics qw/combinations permutations variations/;
+use POSIX qw/ceil floor/;
+use Clone qw/clone/;
+$; = ',';
+
+$_ = read_file(\*STDIN, chomp => 1);
+my @l = /(\d+)/g;
+
+my @a = sort { $a <=> $b } pairkeys @l;
+my @b = sort { $a <=> $b } pairvalues @l;
+my %h = frequency @b;
+
+say "Stage 1: ", sum pairwise { abs($a - $b) } @a, @b;
+say "Stage 2: ", sum map { $_ * ($h{$_} // 0) } @a;
