dddd71f60dfd514f680619fb4291a86cc5384d53
Marcin Gryszkalis <mg@fork.pl>
2024-12-05 01:53:24 +0100

    add template

---
+++ template.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use feature qw/state signatures say multidimensional/;
+use File::Slurp;
+use Data::Dumper;
+use List::Util qw/min max first sum sum0 product all any uniq head tail reduce pairs pairmap pairkeys pairvalues zip mesh/;
+use List::MoreUtils qw/firstidx frequency mode pairwise slide minmax minmaxstr/;
+# use Algorithm::Combinatorics qw/combinations permutations variations/;
+use POSIX qw/ceil floor/;
+use Clone qw/clone/;
+$; = ',';
+
+my @f = read_file(\*STDIN, chomp => 1);
+
+my $stage1 = 0;
+my $stage2 = 0;
+
+for (@f)
+{
+    $_;
+}
+
+say "Stage 1: ", $stage1;
+say "Stage 2: ", $stage2;
