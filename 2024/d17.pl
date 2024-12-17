e9c9eaa5ee56db1d2cb4df3b164831f6d0409e79
Marcin Gryszkalis <mg@fork.pl>
2024-12-17 08:46:05 +0100

    day 17

---
+++ 2024/d17.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use feature qw/state signatures say multidimensional/;
+use File::Slurp;
+use Data::Dumper;
+use List::Util qw/min max first sum sum0 product all any uniq head tail reduce pairs pairmap pairgrep pairfirst pairkeys pairvalues zip mesh/;
+use List::MoreUtils qw/firstidx frequency mode pairwise slide minmax minmaxstr/;
+# use Algorithm::Combinatorics qw/combinations permutations variations/;
+use POSIX qw/ceil floor/;
+use Clone qw/clone/;
+$; = ',';
+
+my $f = read_file(\*STDIN, chomp => 1);
+my @code = $f =~ m/(\d+)/g;
+my $A = shift @code;
+my $B = shift @code;
+my $C = shift @code;
+
+sub combo($a)
+{
+    return $a if $a < 4;
+    return $A if $a == 4;
+    return $B if $a == 5;
+    return $C if $a == 6;
+    die;
+}
+
+my $stage1 = 0;
+my $stage2 = 0;
+
+my @out;
+
+my $ip = 0;
+while (1)
+{
+    my $cmd = $code[$ip];
+    last unless defined $cmd;
+
+    my $op = $code[$ip+1];
+
+    say("$ip: $A $B $C -- ", join(",", @out));
+    if ($cmd == 0) # adv
+    {
+        $A = int($A / (2 ** combo($op)));
+    }
+    elsif ($cmd == 1) # bxl
+    {
+        $B = $B ^ $op;
+    }
+    elsif ($cmd == 2) # bst
+    {
+        $B = combo($op) % 8;
+    }
+    elsif ($cmd == 3) # jnz
+    {
+        if ($A != 0)
+        {
+            $ip = $op;
+            next;
+        }
+    }
+    elsif ($cmd == 4) # bxc
+    {
+        $B = $B ^ $C;
+    }
+    elsif ($cmd == 5) # out
+    {
+        push(@out, combo($op) % 8)
+    }
+    elsif ($cmd == 6) # bdv
+    {
+        $B = int($A / (2 ** combo($op)));
+    }
+    elsif ($cmd == 7) # cdv
+    {
+        $C = int($A / (2 ** combo($op)));
+    }
+    else
+    {
+        die;
+    }
+
+    $ip += 2;
+}
+
+$stage1 = join(",", @out);
+say "Stage 1: ", $stage1;
+say "Stage 2: ", $stage2;
