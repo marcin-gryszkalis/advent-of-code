f308914dc52d4f5485a082b4a7745e3f8fc7dcb8
Marcin Gryszkalis <marcin.gryszkalis@gft.com>
2025-12-11 17:12:48 +0100

    cleanup

--- 2025/d11.pl
+++ 2025/d11.pl
@@ -16,33 +16,21 @@ my @l = read_file(\*STDIN, chomp => 1);
 my $t;
 for (@l)
 {
-    die unless /(.*):\s+(.*)/;
-    my $f = $1;
-    my @d = split/\s+/, $2;
-    for (@d)
-    {
-        $t->{$f}->{$_} = 1;
-    }
+    my @d = split/\W+/;
+    my $f = shift @d;
+    $t->{$f}->{$_} = 1 for @d;
 }
 
 my $gcache;
 sub g($node, $finish, $start = 0)
 {
     $gcache = undef if $start == 1;
-
     return 1 if $node eq $finish;
-
-    my $vs = 0;
-    for my $k (keys %{$t->{$node}})
-    {
-        $vs += $gcache->{$k} // g($k, $finish);
-    }
-
-    return $gcache->{$node} = $vs;
+    return 0 unless exists $t->{$node};
+    return $gcache->{$node} = sum map { $gcache->{$_} // g($_, $finish) } keys %{$t->{$node}};
 }
 
 say "Stage 1: ", g('you', 'out', 1);
 say "Stage 2: ",
     g('svr', 'dac', 1) * g('dac', 'fft', 1) * g('fft', 'out', 1) +
     g('svr', 'fft', 1) * g('fft', 'dac', 1) * g('dac', 'out', 1);
-
